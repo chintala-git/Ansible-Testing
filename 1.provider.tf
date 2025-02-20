6b31cdcc40088181ddbe33d5c3d898e13118238b
mahesh <chintalaa.mahesh@gmail.com>
2025-02-21 00:08:46 +0530

    added bucket name

--- 1.provider.tf
+++ 1.provider.tf
@@ -11,7 +11,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "workspacesbucket001"
+    bucket = "mahesh379379"
     key    = "Ansible.tfstate"
     region = "us-east-1"
     # dynamodb_table = "-terraform-locks"
